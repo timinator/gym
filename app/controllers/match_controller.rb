1f2c0883f9136cab15d80147b5d7c378533a5f89
tjy86 <thomasjyang@gmail.com>
2012-12-09 21:19:45 -0500

    fixed search

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,21 +1,16 @@
 class MatchController < ApplicationController
-  
 	def search
-
-	    query = params[:query]
-	    if query.present?
-	      @users = User.text_search(query)
-	    else
-	      @users = User.all
-	    end
-	    binding.pry
-  end	
-
+    query = params[:query]
+    if query.present?
+      @users = User.text_search(query)
+    else
+      @users = User.all
+    end
+  end
 
   def index
     @users = User.all
   end
   def filter
-    
   end
 end
