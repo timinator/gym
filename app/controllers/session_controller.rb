b050b2dc0aac6010eec4179ba2a7ad2466726eca
tjy86 <thomasjyang@gmail.com>
2012-12-13 09:34:15 -0500

    login on now

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,14 +2,13 @@ class SessionController < ApplicationController
   def new
   end
   def create
-      @user = User.find_by_email(params[:email])
-
-      if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to users_path
-      else
-        render :new
-      end
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to users_path
+    else
+      render :new
+    end
   end
   def destroy
     session[:user_id] = nil
