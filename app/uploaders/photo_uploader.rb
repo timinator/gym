99314dfa11f0d538377c157247cd7f644ca11644
Aaron Fuch <aaronfuchs100@gmail.com>
2012-12-15 01:01:45 +0200

    style - photos, profile, icons

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -44,6 +44,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [40, 40]
   end
 
+  version :big do
+    process :resize_to_fill => [700, 250]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
