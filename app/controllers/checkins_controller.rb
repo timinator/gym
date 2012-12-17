9abab005e173ce36ccc28bb0639b696d01a797fd
tjy86 <thomasjyang@gmail.com>
2012-12-17 11:21:23 -0500

    11 checkins

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,6 +1,6 @@
 class CheckinsController < ApplicationController
   def index
-    @checkins = Checkin.last(5)
+    @checkins = Checkin.last(11)
     @count = Checkin.where(:user_id => session[:user_id]).count
     @motivations = Motivation.all
 
@@ -10,7 +10,7 @@ class CheckinsController < ApplicationController
     t = Time.parse(params[:time]+params[:ampm])
     w=params[:workout]
     Checkin.create(:time => t, :workout_type => w, :user_id => session[:user_id])
-    @checkins = Checkin.last(10)
+    @checkins = Checkin.last(11)
     @count = Checkin.where(:user_id => session[:user_id]).count
 
   end
