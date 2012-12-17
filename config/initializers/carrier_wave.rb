fd4b0dd0cfe887376cfcc7b31642de440eb3f330
tjy86 <thomasjyang@gmail.com>
2012-12-17 09:58:40 -0500

    css file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWSSEC'],       # required
     :region                 => 'us-east-1'                   # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'gym-app'                     # required
+  config.fog_directory  = ENV['AWSGYM']                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
